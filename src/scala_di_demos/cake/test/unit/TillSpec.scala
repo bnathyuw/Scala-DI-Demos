2bb9077b4fdf3f4fb51b53094dcefbb920336c2f
Matthew Butt <matthew.butt@theguardian.com>
2015-10-01 17:21:43 +0100

    Revised TillSpec, removed interim tests

--- src/scala_di_demos/cake/test/unit/TillSpec.scala
+++ src/scala_di_demos/cake/test/unit/TillSpec.scala
@@ -2,49 +2,28 @@ package scala_di_demos.cake.test.unit
 
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala_di_demos.cake.imp.{MessageCreatorComponent, BasketPricerComponent, DefaultTillComponent}
+import scala_di_demos.cake.imp.{BasketPricerComponent, DefaultTillComponent, MessageCreatorComponent}
 
 class TillSpec extends FlatSpec with Matchers with DefaultTillComponent with BasketPricerComponent with MessageCreatorComponent {
 
-  var actualBasket: String = _
-  var actualBasketAndPrice: (String, Int) = _
+  private val expectedBasket: String = "ABCD"
+  private val expectedPrice: Int = 50
+  private val expectedMessage: String = "Expected message"
 
   override def basketPricer = new BasketPricer {
-    override def price(basket: String): Int = {
-      actualBasket = basket
-      50
+    override def price(basket: String): Int = basket match {
+      case `expectedBasket` => expectedPrice
     }
   }
 
   override def messageCreator: MessageCreator = new MessageCreator {
-    def create(basket: String, price: Int): String = {
-      actualBasketAndPrice = (basket, price)
-      "Expected message"
+    def create(basket: String, price: Int): String = (basket, price) match {
+      case (`expectedBasket`, `expectedPrice`) => expectedMessage
     }
   }
 
-  "till" should "get price of basket" in {
-    val basket = "ABCD"
-
-    till.checkOut(basket)
-
-    actualBasket should be(basket)
-  }
-
-  it should "get an output message" in {
-    val basket = "ABCD"
-
-    till.checkOut(basket)
-
-    actualBasketAndPrice should be (basket, 50)
-  }
-
-  it should "return the generated output message" in {
-    val basket = "ABCD"
-
-    val message = till.checkOut(basket)
-
-    message should be ("Expected message")
+  "till" should "return the a message constructed from the basket and its price" in {
+    till checkOut expectedBasket should be(expectedMessage)
   }
 }
 
