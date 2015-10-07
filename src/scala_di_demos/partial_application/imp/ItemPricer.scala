d69cb5c20fa95b2bb3e9d8b5df49d95be4e39852
Matthew Butt <matthew.butt@theguardian.com>
2015-10-07 13:55:30 +0100

    Refactor case statements to partial functions in ItemPricer

--- src/scala_di_demos/partial_application/imp/ItemPricer.scala
+++ src/scala_di_demos/partial_application/imp/ItemPricer.scala
@@ -1,12 +1,19 @@
 package scala_di_demos.partial_application.imp
 
 object ItemPricer {
-  def price: (Char, Int) => Int = {
-    (item, count) => item match {
-      case 'A' => count / 3 * 120 + count % 3 * 50
-      case 'B' => count / 2 * 45 + count % 2 * 30
-      case 'C' => count * 20
-      case 'D' => count * 15
-    }
+  val priceAs: PartialFunction[(Char, Int), Int] = {
+    case ('A', c) => c / 3 * 120 + c % 3 * 50
   }
+  val priceBs: PartialFunction[(Char, Int), Int] = {
+    case ('B', c) => c / 2 * 45 + c % 2 * 30
+  }
+  val priceCs: PartialFunction[(Char, Int), Int] = {
+    case ('C', c) => c * 20
+  }
+  val priceDs: PartialFunction[(Char, Int), Int] = {
+    case ('D', c) => c * 15
+  }
+
+  def price(item: Char, count: Int): Int = (priceAs orElse priceBs orElse priceCs orElse priceDs)(item, count)
+
 }
