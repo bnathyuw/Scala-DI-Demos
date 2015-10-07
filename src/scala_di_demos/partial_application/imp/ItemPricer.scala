e730ac8c6f6143f37cd604f2e7ba5a7bcbae1a9c
Matthew Butt <matthew.butt@theguardian.com>
2015-10-07 17:46:36 +0100

    Refactor item pricer to remove duplication

--- src/scala_di_demos/partial_application/imp/ItemPricer.scala
+++ src/scala_di_demos/partial_application/imp/ItemPricer.scala
@@ -1,19 +1,16 @@
 package scala_di_demos.partial_application.imp
 
 object ItemPricer {
-  val priceAs: PartialFunction[(Char, Int), Int] = {
-    case ('A', c) => c / 3 * 120 + c % 3 * 50
-  }
-  val priceBs: PartialFunction[(Char, Int), Int] = {
-    case ('B', c) => c / 2 * 45 + c % 2 * 30
-  }
-  val priceCs: PartialFunction[(Char, Int), Int] = {
-    case ('C', c) => c * 20
-  }
-  val priceDs: PartialFunction[(Char, Int), Int] = {
-    case ('D', c) => c * 15
+  private def discountPrice(item: Char, itemPrice:Int, discountCount: Int, discountPrice: Int): PartialFunction[(Char, Int), Int] = {
+    case(`item`, count) => count / discountCount * discountPrice + count % discountCount * itemPrice
   }
 
-  def price(item: Char, count: Int): Int = (priceAs orElse priceBs orElse priceCs orElse priceDs)(item, count)
+  private def simplePrice(item: Char, itemPrice: Int): PartialFunction[(Char, Int), Int] = {
+    case(`item`, count) => count * itemPrice
+  }
+
+  private val rules: PartialFunction[(Char, Int), Int] = discountPrice('A', 50, 3, 120) orElse discountPrice('B', 30, 2, 45) orElse simplePrice('C', 20) orElse simplePrice('D', 15)
+
+  def price(item: Char, count: Int): Int = rules(item, count)
 
 }
