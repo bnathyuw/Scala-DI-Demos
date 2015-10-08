a3f0fc7bf6822c630814a003f756ed7c014473ce
Matthew Butt <matthew.butt@theguardian.com>
2015-10-08 17:12:31 +0100

    And put them in their own files

--- src/scala_di_demos/partial_application/test/unit/SimplePriceSpec.scala
+++ src/scala_di_demos/partial_application/test/unit/SimplePriceSpec.scala
@@ -20,33 +20,3 @@ class SimplePriceSpec extends FlatSpec with Matchers {
   }
 }
 
-class DiscountPriceSpec extends FlatSpec with Matchers {
-  val item = 'Z'
-  val expectedPrice = 3
-  val discountCount: Int = 7
-  val discountPrice: Int = 11
-
-  val price: PartialFunction[(Char, Int), Int] = ItemPricer.discountPrice(item, expectedPrice, discountCount, discountPrice)
-
-  "discount price" should "price one item correctly" in {
-    price(item, 1) should be (expectedPrice)
-  }
-
-  it should "price an undiscounted group of items correctly" in {
-    val numberOfItems: Int = 2
-    price(item, numberOfItems) should be (expectedPrice * numberOfItems)
-  }
-
-  it should "price a discount correctly" in {
-    price(item, discountCount) should be (discountPrice)
-  }
-
-  it should "price a group of items part of which gets a discount correctly" in {
-    price(item, discountCount + 1) should be (discountPrice + expectedPrice)
-  }
-
-  it should "price several discounts correctly" in {
-    val numberOfDiscounts: Int = 13
-    price(item, discountCount * numberOfDiscounts) should be (discountPrice * numberOfDiscounts)
-  }
-}
