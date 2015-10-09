60ae288485fd3027c4f880ff71091b731a6a176a
Matthew Butt <matthew.butt@theguardian.com>
2015-10-09 15:10:55 +0100

    Refactor Till to use Try, rather than try...catch

--- src/scala_di_demos/partial_application/imp/Till.scala
+++ src/scala_di_demos/partial_application/imp/Till.scala
@@ -1,11 +1,12 @@
 package scala_di_demos.partial_application.imp
 
+import scala.util.{Failure, Success, Try}
+
 object Till {
   def checkOut(getPrice: String => Int, createMessage: (String, Int) => String)(basket: String): String = {
-    try{
-    createMessage(basket, getPrice(basket))}
-    catch{
-      case UnknownItem(item) => f"Unexpected item in bagging area: $item"
+    Try(getPrice(basket)) match {
+      case Success(price) => createMessage(basket, price)
+      case Failure(UnknownItem(item)) => f"Unexpected item in bagging area: $item"
     }
   }
 }
